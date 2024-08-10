2a110499db4dc81bb7e6eee43dcd1cd3faa3ecf7
Yue Li <lhzuigao@Hotmail.com>
2024-08-10 17:00:28 +0800

    update

--- app.py
+++ app.py
@@ -121,4 +121,4 @@ app.layout = dbc.Container(
 register_callbacks(app)
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run_server(debug=False, dev_tools_ui=False, dev_tools_props_check=False)
