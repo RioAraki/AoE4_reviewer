fe5827adc884c38f782f8f896dac486ec2cb9aca
Yue Li <lhzuigao@Hotmail.com>
2024-08-10 17:22:54 +0800

    minor update

--- app.py
+++ app.py
@@ -123,4 +123,4 @@ register_callbacks(app)
 server = app.server
 
 if __name__ == "__main__":
-    app.run_server(debug=False, dev_tools_ui=False, dev_tools_props_check=False)
+    app.run_server(debug=True)
